165b5082e72369d6001a8e99c3a84dc828ae4f97
huji3156@gmail.com <huji3156@gmail.com>
2020-12-12 23:20:34 +0900

    Change API URL

--- client/movie_app/src/components/SearchBar.vue
+++ client/movie_app/src/components/SearchBar.vue
@@ -12,7 +12,7 @@
 </template>
 
 <script>
-const BASE_URL = 'http://localhost:8000/api/v1/movie_community/all_movies/'   
+const BASE_URL = 'http://3.35.18.1:8000/api/v1/movie_community/all_movies/'   
 import axios from 'axios'
 
 export default {
