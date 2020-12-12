db3e52abf7a3f18ca499d10bc57f5ed54f364d0d
huji3156@gmail.com <huji3156@gmail.com>
2020-12-12 22:51:25 +0900

    Allow cors origin

--- server/movie_app/movie_app/settings.py
+++ server/movie_app/movie_app/settings.py
@@ -14,6 +14,7 @@ SECRET_KEY = '!y(!prs^0o4*mt%cv-po#infe40%_^*8$46#a(0medyg+3q&ra'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
