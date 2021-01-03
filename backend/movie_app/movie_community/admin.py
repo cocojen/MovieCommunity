0ed7ac95555d5972dc4dfb248360077c603e2c98
huji3156@gmail.com <huji3156@gmail.com>
2021-01-03 21:09:26 +0900

    User모델을 admin site 에 등록

--- backend/movie_app/movie_community/admin.py
+++ backend/movie_app/movie_community/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from accounts.models import User
 from .models import Movie, Review, Review_Comment
 
-admin.site.register(User)
 admin.site.register(Movie)
 admin.site.register(Review)
 admin.site.register(Review_Comment)
